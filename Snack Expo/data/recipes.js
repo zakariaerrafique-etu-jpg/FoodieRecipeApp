b1f914e55d71c3559de38fc0eca78c2089d99366
ERRAFIQUE ZAKARIA <zakaria.errafique-etu@etu.univh2c.ma>
2026-07-27 22:29:38 +0200

    Add FlatList and recipe data to recipes.js

--- Snack Expo/data/recipes.js
+++ Snack Expo/data/recipes.js
@@ -11,4 +11,49 @@ export const categories=[
 "Moroccan",
 "Drinks",
 "Vegetarian"
-];
+];  
+<FlatList
+horizontal
+data={categories}
+renderItem={({item})=>
+<CategoryCard title={item}/>
+}
+
+
+{
+id:1,
+
+name:"Chicken Tajine",
+
+image:"https://...",
+
+category:"Moroccan",
+
+ingredients:[
+"Chicken",
+"Olive",
+"Potato"
+],
+
+instructions:[
+"Cut ingredients",
+"Cook slowly",
+"Serve hot"
+],
+
+time:"45 min",
+
+servings:4,
+
+calories:650,
+
+difficulty:"Medium"
+}
+
+  const filteredRecipes =
+recipes.filter(
+item=>item.category===selectedCategory
+);
+
+
+/>
